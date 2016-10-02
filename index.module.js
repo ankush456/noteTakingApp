bada41fc36ad6332c1653319d59d631a76466222
Ankush <ankush@motorzo.in>
2016-10-02 13:00:35 +0530

    undo some changes

--- index.module.js
+++ index.module.js
@@ -7,27 +7,6 @@
      */
     angular
         .module('test', [
-
-            'ngAnimate',
-            'ngAria',
-            'ngCookies',
-            'ngMessages',
-            'ngResource',
-            'ngSanitize',
-            'ngMaterial',
-            'angular-chartist',
-            'chart.js',
-            'datatables',
-            'gridshore.c3js.chart',
-            'nvd3',
-            'pascalprecht.translate',
-            'timer',
-            'ui.router',
-            'uiGmapgoogle-maps',
-            'textAngular',
-            'ui.sortable',
-            'ng-sortable',
-            'xeditable',
-            'moment-picker'
+            'app.notes'
         ]);
 })();
